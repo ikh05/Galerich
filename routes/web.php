a7e1060dfded6a9c1346d9ee6a11d6eda6b21fc4
ikh05 <mi3061923002@gmail.com>
2024-10-14 20:55:17 +0800

    tes

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SearchController;
-
-use App\Models\User;
-use App\Models\Image;
+use App\Http\Controllers\ImageController;
 
 Route::get('/', [UserController::class, 'index']);
 Route::get('/person/{user:username}', [UserController::class, 'person']);
@@ -15,13 +13,11 @@ Route::get('/about', function(){
     return view('about', ['title' => 'About']);
 });
 
-Route::get('/gallery', function(){
-    return view('gallery', [
-        'title' => 'Gallery',
-        'images' => Image::all()
-    ]);
-});
+Route::get('/gallery', [ImageController::class, 'index']);
 
 Route::get('/search', [SearchController::class, 'index']);
 
 Route::get('/login', [LoginController::class, 'index']);
+
+// TES
+Route::get('/tes/1', 'tes');
